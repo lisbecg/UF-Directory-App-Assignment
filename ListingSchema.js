880e8fbe3462f70658928b4b451d032da337b616
lisbeth <lisbecg@gmail.com>
2017-08-31 14:29:17 -0400

    Listing schema was added.

--- ListingSchema.js
+++ ListingSchema.js
@@ -4,7 +4,13 @@ var mongoose = require('mongoose'),
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here */
+  code: String,
+  name: String,
+  coordinates: {
+  	latitude: Number,
+  	longitude: Number
+  },
+  address: String
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
